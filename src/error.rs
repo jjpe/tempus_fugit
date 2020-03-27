53bd0f0fd42ca3a5b79b710937da7ef18b8a035b
Joey Ezechiels <joey.ezechiels@gmail.com>
2020-03-27 21:45:53 +0100

    Remove now-obsolete error handling code for std::num::ParseIntError

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,13 @@
-#[cfg(feature = "enable_serde")] use serde_derive::{Deserialize, Serialize};
-use std::num::ParseIntError;
-
+#[cfg(feature = "enable_serde")]
+use serde_derive::{Deserialize, Serialize};
 
 pub type MeasureResult<T> = Result<T, MeasureErr>;
 
-
 #[cfg(feature = "enable_serde")]
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 pub enum MeasureErr {
     Overflow,
     Underflow,
-    ParseIntError(IntErrorKind),
 }
 
 #[cfg(not(feature = "enable_serde"))]
@@ -19,46 +15,4 @@ pub enum MeasureErr {
 pub enum MeasureErr {
     Overflow,
     Underflow,
-    ParseIntError(IntErrorKind),
-}
-
-
-#[cfg(feature = "enable_serde")]
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-#[derive(Serialize, Deserialize)]
-pub enum IntErrorKind {
-    Empty,
-    InvalidDigit,
-    Overflow,
-    Underflow,
-    Unknown { msg: String },
-}
-
-#[cfg(not(feature = "enable_serde"))]
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum IntErrorKind {
-    Empty,
-    InvalidDigit,
-    Overflow,
-    Underflow,
-    Unknown { msg: String },
-}
-
-
-impl From<ParseIntError> for MeasureErr {
-    fn from(err: ParseIntError) -> MeasureErr {
-        match format!("{}", err).as_str() {
-            "cannot parse integer from empty string" =>
-                MeasureErr::ParseIntError(IntErrorKind::Empty),
-            "invalid digit found in string" =>
-                MeasureErr::ParseIntError(IntErrorKind::InvalidDigit),
-            "number too large to fit in target type" =>
-                MeasureErr::ParseIntError(IntErrorKind::Overflow),
-            "number too small to fit in target type" =>
-                MeasureErr::ParseIntError(IntErrorKind::Underflow),
-            msg => MeasureErr::ParseIntError(IntErrorKind::Unknown {
-                msg: String::from(msg)
-            })
-        }
-    }
 }
